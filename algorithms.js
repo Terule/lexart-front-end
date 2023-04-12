0863dfc42115fe15632988f1ca342c5734dab141
Rafael Aguiar <terule@gmail.com>
2023-04-11 22:35:53 -0300

    feat: 🦾 Created randomAlphanumeric and generateId functions
    randomAlphanumeric returns 4 random characters (numbers and letters) and
    generateId uses randomAlphanumeric to generate the desired pattern

--- algorithms.js
+++ algorithms.js
@@ -37,3 +37,22 @@ const operations = new Map([
   ["multiplication", multiplication],
   ["division", division],
 ]);
+
+/*
+3.  Create an algorithm that generates the following random ID pattern: XXXX-AAAA-BBBB-CCCC
+Where XXXX, AAAA, BBBB and CCCC patterns are random alphanumeric
+The default is a string: "XXXX-AAAA-BBBB-CCCC"
+The result must be stored in a variable. For example:
+$id = generarId()
+id is ~ abc1-bb12-234a-bcc2
+*/
+
+const randomAlphanumeric = () => Math.random().toString(36).substr(2, 4);
+
+const generateId = () => {
+  return `${randomAlphanumeric()}-${randomAlphanumeric()}-${randomAlphanumeric()}-${randomAlphanumeric()}`;
+}
+
+const id = generateId();
+
+console.log(id);
